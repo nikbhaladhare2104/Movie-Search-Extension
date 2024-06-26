79e28a2a462a76961577abd259fb297298ff5ef6
Nikhil Bhaladhare <nikbhaladhare2104@gmail.com>
2024-06-26 20:17:20 +0530

    Added search history display in the popup interface

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,34 @@ import { setMovieDetails } from './store/movieSlice';
 
 function App() {
 const dispatch = useDispatch();
-  const movieDetails = useSelector((state: RootState) => state.movie.details);
-   useEffect(() => {
+  const { details, history } = useSelector((state: RootState) => state.movie);
+
+
+    useEffect(() => {
     chrome.storage.local.get(null, (items) => {
       const latestMovie = Object.values(items)[0];
       dispatch(setMovieDetails(latestMovie));
     });
   }, [dispatch]);
 
-   if (!movieDetails) {
+  if (!details) {
     return <div>Loading...</div>;
   }
 
   return (
     <div>
-      <h1>{movieDetails.title}</h1>
-      <p>Release Date: {movieDetails.release_date}</p>
-      <p>Overview: {movieDetails.overview}</p>
-      <p>Rating: {movieDetails.vote_average}</p>
+      <h1>{details.title}</h1>
+      <p>Release Date: {details.release_date}</p>
+      <p>Overview: {details.overview}</p>
+      <p>Rating: {details.vote_average}</p>
+      <h2>Search History</h2>
+      <ul>
+        {history.map((movie, index) => (
+          <li key={index}>
+            {movie.title} - {movie.release_date}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
